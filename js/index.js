15ed5b73b753fd0ee966823abec922275e86ef61
ABid hasan <abeydhasan134@gmail.com>
2024-03-03 16:26:20 +0600

    search, footer, register and spinner

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 // Function to load data from the API
-const loadData = async () => {
-    try {
-        const resp = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`);
-        const data = await resp.json();
-        AllPosts(data.posts);
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
+const loadData = (searchText='') => {
+    setTimeout(async () => {
+        try {
+            const resp = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts${searchText}`);
+            const data = await resp.json();
+            AllPosts(data.posts);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }, 2000);
 };
 // load fuction end
 
@@ -16,7 +18,7 @@ loadData();
 // Function to create card section based on API data
 const AllPosts = (details) => {
     const postContener = document.getElementById('post-contener');
-    
+    postContener.innerHTML ="";
     // Loop through each post and create card elements
     details.forEach((element) => {
         // Create a new post element for each iteration
@@ -62,7 +64,7 @@ const AllPosts = (details) => {
                         </div>
                     </div>
                     <div>
-                        <button onClick="readedBook()"><i class="fa-regular fa-envelope bg-green-400 px-2 py-1 text-2xl rounded-[50%]"></i></button>
+                        <button onclick="readedBook('${element.id}', '${element.title}', '${element.view_count}')"><i class="fa-regular fa-envelope bg-green-400 px-2 py-1 text-2xl rounded-[50%]"></i></button>
                     </div>
                 </div>
             </div>
@@ -75,8 +77,64 @@ const AllPosts = (details) => {
         // Append the post to the container
         postContener.appendChild(post);
     });
+    // hidening spinner 
+    loadingSpinner(false);
 };
+// function end of all card
 
-const readedBook = ()=>{
-    console.log('readedBook');
+// function readedBook start
+const array=[];
+const readedBook = async(id,title,view)=>{
+    // making all id array
+    const bookNumbers= document.getElementById('bookNumber');
+    array.push(id);
+    // console.log(array.length);
+    bookNumbers.textContent=array.length;
+    
+    // for(let i=0;i)
+    const readContainer = document.getElementById('read-contener')
+    // console.log(id,title,view);
+    const readBook=document.createElement('div');
+    readBook.innerHTML=`
+    <div class="p-5 flex justify-between my-3 bg-white shadow-md rounded-lg">
+                    <div>
+                        <h1 class="text-4xl">${title}</h1>
+                    </div>
+                    <div class="flex items-center gap-3">
+                        <i class="fa-solid fa-eye text-2xl"></i>
+                        <p>${view}</p>
+                    </div>
+            </div>
+    `
+    readContainer.appendChild(readBook);
+   
+    
 }
+// fucntion readedBook end
+
+// handle search start
+const handelsearch=()=>{
+    loadingSpinner(true);
+    const searchField=document.getElementById('search-fild');
+    const searchItem=searchField.value;
+    const sendItem=`?category=${searchItem}`
+    // console.log(sendItem);
+    loadData(sendItem);
+    searchField.value = '';
+    // console.log(searchItem)
+}
+// handle search end
+
+// adding spinner to search start
+const loadingSpinner=(isLoading)=>{
+    const spinner=document.getElementById('loading-Spinner');
+    if(isLoading){
+        spinner.classList.remove('hidden');
+    }
+    else{
+        spinner.classList.add('hidden');
+    }
+
+}
+// adding spinner to search end
+
